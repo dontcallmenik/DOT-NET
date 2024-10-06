6b71e8739a31ef0fb9337c74e34479ef6a112a6d
Nikita Feshchenko <114148391+dontcallmenik@users.noreply.github.com>
2024-10-06 22:28:49 +0300

    Add files via upload

---
+++ Labs/Lab01/Task05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Введення координат точки
+        Console.Write("Введіть координату x: ");
+        double x = double.Parse(Console.ReadLine());
+
+        Console.Write("Введіть координату y: ");
+        double y = double.Parse(Console.ReadLine());
+
+        // Перевірка, чи точка потрапляє у прямокутник
+        bool isInRectangle = (x >= 0 && x <= 1 && y >= 0 && y <= 1);
+
+        // Перевірка, чи точка потрапляє у півколо
+        bool isInSemicircle = (x * x + y * y <= 1 && y <= 0);
+
+        // Якщо точка належить або до прямокутника, або до півкола
+        if (isInRectangle || isInSemicircle)
+        {
+            Console.WriteLine("Точка потрапляє в задану область.");
+        }
+        else
+        {
+            Console.WriteLine("Точка не потрапляє в задану область.");
+        }
+    }
+}
